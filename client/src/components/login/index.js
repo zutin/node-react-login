ee4d857f718687ea1c8247453092af1a1ea3c0a8
zutin <gianluccaz@hotmail.com>
2023-08-22 17:37:19 -0300

    finished project

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -42,36 +42,52 @@ export function Login() {
     return (
         <>
         {user == null ? (
-            <div className="w-1/2 bg-slate-400 sm:bg-black min-h-screen flex justify-center min-w-full sm:min-w-fit text-slate-200">
-                <div className="rounded-xl bg-slate-900 p-8 my-auto">
-                <span className="text-2xl font-bold">Acesse o sistema</span>
-                    <form>
-                        <div className="mt-4">
-                            <label htmlFor="username" className="flex font-medium mb-1">Usuário</label>
-                            <input type="text" name="username" onChange={(e) => setUsername(e.target.value)} required className="rounded-xl p-2 text-black active:ring-red-500" placeholder="Insira seu usuário"></input>
-                        </div>
-                        <div className="mt-4">
-                            <label htmlFor="password" className="flex font-medium mb-1">Senha</label>
-                            <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} required className="rounded-xl p-2 text-black active:ring-red-500" placeholder="Digite sua senha"></input>
-                        </div>
-                        <div className="mt-6">
-                            <button type="submit" onClick={(e) => handleLogin(e)} className={"p-4 flex mx-auto rounded-xl bg-gray-700 transition hover:bg-gray-800 active:bg-blue-700"}>
-                                Acessar
-                            </button>
-                        </div>
-                    </form>
-                <p className="text-red-600 text-center mt-4">{error}</p>
+            <>
+                <div className="w-2/3">
+                    <div className="h-full overflow-hidden relative">
+                    <img src='https://cdn.pixabay.com/photo/2019/02/21/18/36/city-4011922_960_720.jpg' alt="login" className="w-full h-full object-cover"></img>
+                    <div className="absolute inset-0 -left-3/4 bg-gradient-to-l from-black via-transparent to-transparent"></div>
+                    </div>
                 </div>
-            </div>
+                <div className="w-1/2 bg-slate-400 sm:bg-black min-h-screen flex justify-center min-w-full sm:min-w-fit text-slate-200">
+                    <div className="rounded-xl bg-slate-900 p-8 my-auto">
+                    <span className="text-2xl font-bold">Acesse o sistema</span>
+                        <form>
+                            <div className="mt-4">
+                                <label htmlFor="username" className="flex font-medium mb-1">Usuário</label>
+                                <input type="text" name="username" onChange={(e) => setUsername(e.target.value)} required className="rounded-xl p-2 text-black" placeholder="Insira seu usuário"></input>
+                            </div>
+                            <div className="mt-4">
+                                <label htmlFor="password" className="flex font-medium mb-1">Senha</label>
+                                <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} required className="rounded-xl p-2 text-black" placeholder="Digite sua senha"></input>
+                            </div>
+                            <div className="mt-6">
+                                <button type="submit" onClick={(e) => handleLogin(e)} className={"p-4 flex mx-auto rounded-xl bg-gray-700 transition hover:bg-gray-800 active:bg-blue-700"}>
+                                    Acessar
+                                </button>
+                            </div>
+                        </form>
+                    <p className="text-red-600 text-center mt-4">{error}</p>
+                    </div>
+                </div>
+            </>
         ) : (
-            <div className='w-1/2 bg-black flex flex-col'>
-                <div className='text-center my-auto'>
-                    <span className="text-slate-100 text-3xl">Acessou como <b>{user.name}</b>.</span>
-                    <br/>
-                    <span className='text-slate-100 text-3xl'>Seu ID é <b>{user._id}</b>.</span>
+            <>
+                <div className='w-1/2 bg-black flex flex-col'>
+                    <div className='text-center my-auto'>
+                        <span className="text-slate-100 text-3xl">Acessou como <b>{user.name}</b>.</span>
+                        <br/>
+                        <span className='text-slate-100 text-3xl'>Seu ID é <b>{user._id}</b>.</span>
+                    </div>
+                    <button type="button" className="bg-blue-500 transition rounded-xl p-3 w-1/5 mb-4 mx-auto text-slate-100 hover:bg-blue-400 active:bg-blue-600" onClick={(e) => handleLogout(e)}>Voltar</button>
                 </div>
-                <button type="button" className="bg-blue-500 transition rounded-xl p-3 w-1/5 mb-4 mx-auto text-slate-100 hover:bg-blue-400 active:bg-blue-600" onClick={(e) => handleLogout(e)}>Voltar</button>
-            </div>
+                <div className="w-2/3">
+                    <div className="h-full overflow-hidden relative">
+                    <img src='https://cdn.pixabay.com/photo/2017/01/28/02/24/japan-2014619_960_720.jpg' alt="login" className="w-full h-full object-cover"></img>
+                    <div className="absolute inset-0 -right-3/4 bg-gradient-to-r from-black via-transparent to-transparent"></div>
+                    </div>
+                </div>
+            </>
         )}
         </>
     )
